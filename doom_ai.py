c75d72661f0561e5602a7f6b48b4e67d80ba2983
bramstorp <bramstorpmathias@gmail.com>
2021-09-06 11:42:47 +0200

    added parments + q learnings values

--- doom_ai.py
+++ doom_ai.py
@@ -1,5 +1,29 @@
 import vizdoom as vzd
 
+# Q-learing values
+learning_rate = 0.00025
+discount_factor = 0.99
+train_epochs = 5
+learning_steps_per_epoch = 2000
+replay_memory_size = 10000
+
+# Neural Network Batch Size
+batch_size = 64
+
+# Training regime
+test_episodes_per_epoch = 100
+
+# Other parameters
+frame_repeat = 12
+resolution = (30, 45)
+episodes_to_watch = 10
+
+model_savefile = "./model-doom.pth"
+save_model = True
+load_model = False
+skip_learning = False
+
+
 def doom_game_initializer():
     game = vzd.DoomGame()
     game.load_config("scenarios/simpler_basic.cfg")
